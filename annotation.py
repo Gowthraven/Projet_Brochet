e6f619c4e59dc8477e64b7b3e203ba8cb4f20584
Enguerran-Couderc <enguerrancouderc@gmail.com>
2024-03-21 22:51:17 +0100

    annotation12, identification brochet

--- annotation.py
+++ annotation.py
@@ -4,18 +4,23 @@ from os.path import isfile, join
 import numpy as np
 import random
 
-def init(foldername):
+def init(foldername,datatype,filename):
 
-    df = pd.DataFrame(columns=['class_id', 'file', 'segmentation_split'])
+    df = pd.DataFrame(columns=['class_id', 'file', "reid_split", 'segmentation_split'])
     df["file"]=[f for f in listdir(foldername) if isfile(join(foldername, f)) and f.endswith(".JPG")]
-    df.to_csv(foldername+"/annotation.csv", sep=',',index=False)
+    df["reid_split"]=[datatype]*len(df)
+    df.to_csv(filename+".csv", sep=',',index=False)
+    print("Done")
 
-def split(foldername,train_test_val=(0.4,0.2,0.4)):
-    df = pd.read_csv(foldername+"/annotation.csv")
+def split(filename,train_test_val=(0.4,0.2,0.4)):
+    df = pd.read_csv(filename+".csv")
     df=df.sort_values(by=['class_id'])
     class_id=np.unique(df["class_id"])
     random.shuffle(class_id)
     train, validate, test = np.split(class_id,[int(.4*len(class_id)), int(.6*len(class_id))])
+    print(train)
+    print(validate)
+    print(test)
     for i in range(len(df)):
         if np.isin(df.at[i,"class_id"],train):
             df.at[i,"segmentation_split"]="training"
@@ -24,10 +29,10 @@ def split(foldername,train_test_val=(0.4,0.2,0.4)):
         if np.isin(df.at[i,"class_id"],validate):
             df.at[i,"segmentation_split"]="validation"
             
-    df.to_csv(foldername+"/annotation.csv", sep=',',index=False)
+    df.to_csv(filename+".csv", sep=',',index=False)
 
 
 if __name__ == '__main__':
-    foldername=""
-    init(foldername)
-    #split(foldername)
+
+    #init("../CAMPAGNE2-101114/","database","annotation2")
+    split("annotation12")
