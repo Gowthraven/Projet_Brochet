edffa98a656c1365968b68be80e2cee964c3c979
Enguerran-Couderc <enguerrancouderc@gmail.com>
2024-03-22 02:12:35 +0100

    annotation final

--- annotation.py
+++ annotation.py
@@ -15,18 +15,15 @@ def init(foldername,datatype,filename):
 def split(filename,train_test_val=(0.4,0.2,0.4)):
     df = pd.read_csv(filename+".csv")
     df=df.sort_values(by=['class_id'])
-    class_id=np.unique(df["class_id"])
+    class_id=[i for i in range(len(df))]
     random.shuffle(class_id)
     train, validate, test = np.split(class_id,[int(.4*len(class_id)), int(.6*len(class_id))])
-    print(train)
-    print(validate)
-    print(test)
     for i in range(len(df)):
-        if np.isin(df.at[i,"class_id"],train):
+        if np.isin(i,train):
             df.at[i,"segmentation_split"]="training"
-        if np.isin(df.at[i,"class_id"],test):
+        if np.isin(i,test):
             df.at[i,"segmentation_split"]="testing"
-        if np.isin(df.at[i,"class_id"],validate):
+        if np.isin(i,validate):
             df.at[i,"segmentation_split"]="validation"
             
     df.to_csv(filename+".csv", sep=',',index=False)
@@ -34,5 +31,5 @@ def split(filename,train_test_val=(0.4,0.2,0.4)):
 
 if __name__ == '__main__':
 
-    #init("../CAMPAGNE2-101114/","database","annotation2")
+    #init("../CAMPAGNE34/","query","annotation34rectif")
     split("annotation12")
